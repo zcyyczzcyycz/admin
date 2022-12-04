586a46a2a9ca598195f75b963a3c585eab3255aa
zcy <2468687771@qq.com>
2022-12-04 15:44:47 +0800

    “手动初始化了原有的项目配置，删除了写不必要的组件”

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入路由
 import router from './router'
+// 引入vuex
 import store from './store'
+// 引入element ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+// 使用element ui插件
+Vue.use(ElementUI);
+
+// 关闭终端提示
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
